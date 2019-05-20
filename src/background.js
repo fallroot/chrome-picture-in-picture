8ae2de1c007128ccc1e0a33f8b8caeb916db48f1
CK Moon <fallroot@gmail.com>
2019-05-20 12:55:10 +0900

    Add context menus

--- src/background.js
+++ src/background.js
@@ -1,3 +1,26 @@
-chrome.browserAction.onClicked.addListener(tab => {
+const MENU_ID = 'extension.pip'
+
+function run () {
+  if (!document.pictureInPictureEnabled) return
+
+  chrome.browserAction.onClicked.addListener(executeScript)
+
+  chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+      id: MENU_ID,
+      title: 'Picture in Picture'
+    })
+  })
+
+  chrome.contextMenus.onClicked.addListener(info => {
+    if (info.menuItemId === MENU_ID) {
+      executeScript()
+    }
+  })
+}
+
+function executeScript () {
   chrome.tabs.executeScript({ file: 'script.js' })
-})
+}
+
+run()
